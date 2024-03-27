ac7b01d5ae62629e1a08f94f1a1767dd6ae5a092
HarshGajjar013 <gajjarharsh180@gmail.com>
2024-03-27 19:54:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class Assignment03
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("GLS", "Mercedes", 2019, 150, 4, "Petrol", "Automatic", true);
+        Truck truck1 = new Truck("Canyon", "GMC", 2017, 250, 2, "Pickup", false);
+        Motorcycle bike1 = new Motorcycle("ZX-4R", "Kawasaki", 2021, 100, 2000, "Petrol", false);
+
+        Console.WriteLine("Details of car:");
+        car1.DisplayDetails();
+
+        Console.WriteLine("\nDetails of Truck:");
+        truck1.DisplayDetails();
+
+        Console.WriteLine("\nDetails of motorcycle:");
+        bike1.DisplayDetails();
+
+        // displaying total revenue
+        double totalRevenue = car1.RentalPrice + truck1.RentalPrice + bike1.RentalPrice;
+        Console.WriteLine($"\nTotal Revenue of vehicle is: ${totalRevenue:f2}");
+    }
+}
---
+++ RentalAgency.cs
@@ -0,0 +1,44 @@
+﻿public class RentalAgency
+{
+    public Vehicle[] Fleet;
+    public double TotalRevenue { get; private set; }
+
+    public RentalAgency()
+    {
+        Fleet = new Vehicle[0];
+        TotalRevenue = 0;
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Array.Resize(ref Fleet, Fleet.Length + 1);
+        Fleet[Fleet.Length - 1] = vehicle;
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        int index = Array.IndexOf(Fleet, vehicle);
+        if (index != -1)
+        {
+            TotalRevenue += vehicle.RentalPrice;
+            Array.Copy(Fleet, index + 1, Fleet, index, Fleet.Length - index - 1);
+            Array.Resize(ref Fleet, Fleet.Length - 1);
+        }
+    }
+
+    public void RentVehicle(Vehicle vehicle)
+    {
+        int index = Array.IndexOf(Fleet, vehicle);
+        if (index != -1)
+        {
+            TotalRevenue += vehicle.RentalPrice;
+            Array.Copy(Fleet, index + 1, Fleet, index, Fleet.Length - index - 1);
+            Array.Resize(ref Fleet, Fleet.Length - 1);
+            Console.WriteLine($"Vehicle {vehicle.Model} rented successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"Vehicle {vehicle.Model} is not available for rent.");
+        }
+    }
+}
